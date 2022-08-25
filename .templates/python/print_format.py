8d20603579769f48be08f1a403a815f75ca3cbc7
zomorodiyan <mehrdadzomorodiyan@gmail.com>
2022-08-25 16:08:20 -0500

    add print format python template

---
+++ .templates/python/print_format.py
@@ -0,0 +1,10 @@
+x = 100
+y = "100.00"
+print("The value of x is {z:.2f}.".format(z=x))
+print("The value of x is {z:s}.".format(z=y))
+print("The value of x is {z:.2e}.".format(z=x))
+print()
+print(f"The value of x is {x:.2f}.")
+print(f"The value of x is {y:s}.")
+print(f"The value of x is {x:.2e}.")
+# https://docs.python.org/3/library/string.html#formatstrings
