93b0c42de9dc79be7f4a70fed6dcd279de3c788b
zomorodiyan <mehrdadzomorodiyan@gmail.com>
2022-08-25 16:32:55 -0500

    update print_format template

--- .templates/python/print_format.py
+++ .templates/python/print_format.py
@@ -1,8 +1,8 @@
 x = 100
 y = "100.00"
 print("The value of x is {z:.2f}.".format(z=x))
-print("The value of x is {z:s}.".format(z=y))
-print("The value of x is {z:.2e}.".format(z=x))
+print("The value of x is {:s}.".format(y))
+print("The value of x is {:.2e}.".format(x))
 print()
 print(f"The value of x is {x:.2f}.")
 print(f"The value of x is {y:s}.")
